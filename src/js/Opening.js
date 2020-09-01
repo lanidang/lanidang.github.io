2c34cfdf6e399626b6f49a52da082f34668d6b34
lanidang <lanidang@cs.washington.edu>
2020-08-31 22:10:34 -0700

    created an opening page with name

---
+++ src/js/Opening.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../css/Opening.css';
+
+class Opening extends Component {
+    render() {
+        return (
+            <div className="Opening">
+                <header className="Opening-header">
+                    <p className="intro">
+                        hi, i'm lani
+                    </p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Opening;
