a43c9f7b1fef09d9174312930a29374d760f302d
lanidang <lanidang@cs.washington.edu>
2020-09-02 19:21:31 -0700

    update website with opening

---
+++ precache-manifest.47293cec6e4eeb32c9bbb321d0104a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925bb07900f8ddccfa28703a576fd00b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98214e4092d190632bee",
+    "url": "/static/css/main.c24a25fb.chunk.css"
+  },
+  {
+    "revision": "eb768b810dad07913bdc",
+    "url": "/static/js/2.d06f118d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d06f118d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98214e4092d190632bee",
+    "url": "/static/js/main.fb5a7408.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.609edb1dcb31abb957265f099024f94a.js"
+  "/precache-manifest.47293cec6e4eeb32c9bbb321d0104a21.js"
 );
 
 self.addEventListener('message', (event) => {
